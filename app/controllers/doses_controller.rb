8364a654f1592fac26046e471b5e73a31d0f7eb3
Andrew Medhat <skiresstranger@gmail.com>
2021-03-04 17:40:09 +0200

    doses and cocktails done

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,52 +1,53 @@
 class DosesController < ApplicationController
-    before_action :find_dose, only: [ :edit, :update, :edit, :show, :destroy]
-    def index
-      @doses = Dose.all
-    end
+  before_action :find_dose, only: %i[edit update edit show destroy]
+  def index
+    @doses = Dose.all
+  end
 
-    def show
-    end
+  def show; end
 
-    def new
-      @cocktail = Cocktail.find(params[:cocktail_id])
-      @dose = Dose.new
-    end
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
 
-    def create # do with form
-      @cocktail = Cocktail.find(params[:cocktail_id])
-      @dose = Dose.new(dose_params)
-      @dose.cocktail = @cocktail
-      if @dose.save
-        redirect_to cocktail_path(@cocktail)
-      else
-        render :new
-      end
-    end
-
-    def dose_params
-      params.require(:dose).permit(:description, :ingredient_id)
-    end
-
-    def edit
-    end
-
-    def update  # do with form
-      find_dose
-      if @dose.update(dose_params)
-        redirect_to @dose, notice: 'dose was successfully updated.'
-        # redirect_to dose_path
-      else
-        render :edit
-      end
-    end
-    def destroy
-      find_dose
-      @dose.destroy
-      # no need for app/views/doses/destroy.html.erb
-      redirect_to doses_path
-    end
-
-    def find_dose
-      @dose = Dose.find(params[:id])
+  # do with form
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
     end
   end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def edit; end
+
+  # do with form
+  def update
+    find_dose
+    if @dose.update(dose_params)
+      redirect_to @dose, notice: 'dose was successfully updated.'
+      # redirect_to dose_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def find_dose
+    @dose = Dose.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :cocktails do
-    resources :doses, only: [ :new, :create ]
+    resources :doses, only: %i[new create]
   end
-
-  root to: "cocktails#index"
+  delete '/doses/:id', to: 'doses#destroy', as: 'dose'
+  root to: 'cocktails#index'
 end
